bcb12aeaedccd89e5aa14d966a38ff8854467239
Sadiq Ja'afar <jaafarsadiqyusuf@gmail.com>
2025-02-01 15:41:05 +0100

    tailwind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.text}>Welcome to my app</Text>
+      <Link href="/sign-in">SignIn</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="properties/1">Property</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: "bold",
+    color: "red",
+    fontSize: 20, // You can adjust the font size as needed
+  },
+});
